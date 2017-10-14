fa094ab69b7a7f18ab95c53b434b55c0583ed606
Bruno Fulber Wide <bfwide07@gmail.com>
2017-10-14 15:50:50 -0300

    caclulating arithmetic expressions

--- DataTypes.hs
+++ DataTypes.hs
@@ -2,23 +2,22 @@ module DataTypes where
 
 import Prelude
 
+data ArithmeticExpression = 
+    Number Integer 
+    | ArithmeticExpression :+: ArithmeticExpression 
+    | ArithmeticExpression :*: ArithmeticExpression deriving (Eq, Show)
+
 data BooleanExpression = 
     Boolean Bool 
     | And BooleanExpression BooleanExpression 
     | Or BooleanExpression BooleanExpression 
     | Not BooleanExpression deriving (Eq, Show)
 
-data ArithmeticExpression = 
-    Number Integer 
-    | ArithmeticExpression :+: ArithmeticExpression 
-    | ArithmeticExpression :*: ArithmeticExpression deriving (Eq, Show)
-
 data Valor = Integer | Bool deriving (Show, Eq)
 
 data Expression = 
     Nil
     | Atrib Bool String
-    | Atrib Integer String
     | Seq Expression Expression
     | Esc Expression Expression
     | LoopPre BooleanExpression Expression
@@ -29,3 +28,16 @@ calculateBool (Boolean x) = x
 calculateBool (And x y) = (calculateBool x) && (calculateBool y )
 calculateBool (Or x y) = (calculateBool x) || (calculateBool y )
 calculateBool (Not x) = (calculateBool x)
+
+calculateArithmeticExpression::ArithmeticExpression -> Integer
+calculateArithmeticExpression (Number x) = x
+calculateArithmeticExpression (x :+: y) = (calculateArithmeticExpression x) + (calculateArithmeticExpression y)
+calculateArithmeticExpression (x :*: y) = (calculateArithmeticExpression x) * (calculateArithmeticExpression y)
+
+calculateExpression::Expression -> ()
+calculateExpression Nil = ()
+
+--                               ___  __   __                     ___    __  
+-- \  /  /\  |     /\      |\/| |__  |__) |  \  /\      /\  |    |__  | /  \ 
+--  \/  /~~\ |    /~~\     |  | |___ |  \ |__/ /~~\    /~~\ |___ |    | \__/ 
+                                                                          
