638dbd4cf347da0388a2e1f5505083261da99207
Bruno Fulber Wide <bfwide07@gmail.com>
2017-10-18 20:54:20 -0200

    started mult

--- Math.hs
+++ Math.hs
@@ -6,3 +6,6 @@ import Commands
 
 sum :: Integer -> Integer -> Integer
 sum x y =  calculate ( (Number x) :+: (Number y) )
+
+mult :: Integer -> Integer -> Integer
+mult x y = value "mult" (calculateExpression (LoopPre ((Number x) :<: (Number 1)) -- Expression )  
